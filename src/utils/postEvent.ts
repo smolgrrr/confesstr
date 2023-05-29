753c425ed49419ee428c6f5065bd67c07a8e3ec5
smolgrrr <104137257+smolgrrr@users.noreply.github.com>
2023-05-29 13:59:35 +1000

    add tags

--- src/utils/postEvent.ts
+++ src/utils/postEvent.ts
@@ -4,13 +4,28 @@ import {
   getPublicKey,
   getEventHash,
   getSignature,
+  nip19
 } from "nostr-tools";
 
 export const handleThreadSubmit = async (comment: string, hasSubmittedPost: boolean) => {
+  
   if (!comment) {
     alert("no message provided");
     return;
   }
+
+  const tags = [];
+  if (comment.includes('note') || comment.includes('npub')) {
+    const tag = comment.match(/(note|npub)[a-z0-9]+/);
+    
+    if (tag === null) {
+    } else if (tag[0].includes('note')) {
+      tags.push(["e", nip19.decode(tag[0]).data as string]);
+    } else if (tag[0].includes('npub')) {
+      tags.push(["p", nip19.decode(tag[0]).data as string]);
+    }
+  }
+  
 
   if (hasSubmittedPost) {
     alert('You have already submitted a post.');
@@ -21,7 +36,7 @@ export const handleThreadSubmit = async (comment: string, hasSubmittedPost: bool
     id: 'null',
     content: comment,
     kind: 1,
-    tags: [],
+    tags,
     created_at: dateToUnix(),
     pubkey: 'null',
     sig: 'null',
